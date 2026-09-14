82fe277c946d9bb2523d99e35864b321b113649f
Savo <riksavo@gmail.com>
2026-09-14 19:48:17 +0200

    Fix TS4104 error in AIPipeline

--- src/components/AIPipeline.tsx
+++ src/components/AIPipeline.tsx
@@ -71,7 +71,7 @@ function PipelineStep({
 
   const { scrollYProgress } = useScroll({
     target: stepRef,
-    offset: STEP_SCROLL_OFFSET as [any, any],
+    offset: STEP_SCROLL_OFFSET as ["start 32%", "end 18%"],
   });
 
   const opacity = useTransform(
